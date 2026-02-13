104c184d994a1a254c6fe4f7ce0fd6d2eb20f705
qiaofuyo <1103351140@qq.com>
2026-02-13 18:59:19 +0800

    feat: 集成 Supabase 初始化

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://lgthupyclkdiuynyokcn.supabase.co',
+    anonKey: 'sb_publishable_evV5wtX6Ge1U3A1RWwe0AQ_Ch823EKw'
+  );
+
   runApp(const MainApp());
 }
+
+final supabase = Supabase.instance.client; // 全局客户端
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
